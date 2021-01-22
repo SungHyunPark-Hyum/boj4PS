66d45d29612e4cd0a041c98417a205e75e3e5e74
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-01-22 16:36:14 +0900

    2021.01.22_algorithm_sorting

--- Sorting/Merge_sorting.java
+++ Sorting/Merge_sorting.java
@@ -9,11 +9,48 @@ public class Merge_sorting {
     public static int[] arr = {5, 6, 2, 8, 7, 23, 4, 1, 10};
     public static int[] sorted_arr = new int[9];
 
-    public static void merge_sort_ascend(int[] arr) {
+    public static void merge_sort(int[] arr, int l, int r) {
+        if(l < r) {
+            int mid = (l+r)/2;
+            merge_sort(arr, 0, mid);
+            merge_sort(arr, mid+1, r);
+            System.out.println("l = " + l + " / mid = " + mid + " / r = " + r);
+            merge(arr, l, mid, r);
+        }
+    }
 
+    public static void merge(int[] arr, int l, int mid, int r){
+        int[] tmp = new int[arr.length];
+        int i = l;
+        int j = mid+1;
+        int k = l;
+
+        while(i < mid+1 && j < r+1){
+            if(arr[i] < arr[j]){
+                tmp[k++] = arr[i++];
+            }
+            else{
+                tmp[k++] = arr[j++];
+            }
+        }
+
+        while(i<=mid){
+            tmp[k++] = arr[i++];
+        }
+        while(j<=r){
+            tmp[k++] = arr[j++];
+        }
+
+        for(int t=l; t<=r; ++t){
+            arr[t] = tmp[t];
+        }
     }
 
     public static void main(String[] args) {
-        merge_sort_ascend(arr);
+        merge_sort(arr, 0, arr.length-1);
+        System.out.println("sorted array : ");
+        for(int i=0; i<arr.length; ++i){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
---
+++ Sorting/Quick_sorting.java
@@ -0,0 +1,75 @@
+package Sorting;
+
+// 최악 : O(n^2)
+// 최선 : O(n log(n))
+
+// divide and conquer
+// use pivot
+// use recursion
+
+public class Quick_sorting {
+    public static int[] arr = {5, 6, 2, 8, 7, 23, 4, 1, 10};
+
+    public static void quick_sort(int[] arr, int l, int r){
+        if(l < r){
+
+            int q = partition(arr, l, r);
+            quick_sort(arr, l, q-1);
+            quick_sort(arr, q+1, r);
+        }
+    }
+
+    // arr = {5, 6, 2, 8, 7, 23, 4, 1, 10};
+
+    //         /    i  j            p /
+    // arr = {5, 6, 2, 8, 1, 23, 4, 7, 10};
+
+    //         /    i     j         p /
+    // arr = {5, 6, 2, 8, 1, 23, 4, 7, 10};
+
+    //         /       i      j     p /
+    // arr = {5, 6, 2, 1, 8, 23, 4, 7, 10};
+
+    //         /       i         j  p /
+    // arr = {5, 6, 2, 1, 8, 23, 4, 7, 10};
+
+    //         /          i      j  p /
+    // arr = {5, 6, 2, 1, 4, 23, 8, 7, 10};
+
+    //         /             p        /
+    // arr = {5, 6, 2, 1, 4, 7, 23, 8, 10};
+
+    // pivot = r
+    public static int partition(int[] arr, int l, int r){
+        int p = r;
+        int pivot = arr[p];
+        int i = l-1;
+        int j = l;
+
+        while(j<r){
+            if(arr[j] > pivot){
+                j++;
+            }
+            else{
+                i++;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j++] = tmp;
+            }
+        }
+        arr[p] = arr[i+1];
+        arr[i+1] = pivot;
+        p = i+1;
+
+        return p;
+    }
+
+    public static void main(String[] args) {
+        quick_sort(arr, 0, arr.length-1);
+        System.out.println("sorted array : ");
+        for(int i=0; i<arr.length; ++i){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
