d62a7c1e7a3a7d4056e29cf46f1066ad8cee2b0f
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-01-24 15:06:58 +0900

    2021.01.24 - Sorting

---
+++ boj1181.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+// BOJ 1181 단어 정렬 <정렬 단계>
+public class boj1181 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        ArrayList<String> arr = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; ++i){
+            String str = br.readLine();
+            if(!arr.contains(str))
+                arr.add(str);
+        }
+
+        Comparator<String> cmp = new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                if(s1.length() != s2.length())
+                    return s1.length() - s2.length();
+                else
+                    return s1.compareTo(s2);
+            }
+        };
+
+        Collections.sort(arr, cmp);
+
+        for(int i=0; i<arr.size(); ++i)
+            sb.append(arr.get(i)).append("\n");
+
+        System.out.println(sb);
+    }
+}
