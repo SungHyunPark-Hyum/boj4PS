4b7b197241b5a1fbcf0496da556461dbc5301440
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-01-25 17:26:09 +0900

    2021.01.25 - Back Tracking

---
+++ boj15649.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+// BOJ 15649 N과 M(1) <백트래킹 단계>
+public class boj15649 {
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void nNm(int n, int m, ArrayList<Integer> arr){
+        if(m==0){
+            for(int i=0; i<arr.size(); ++i){
+                sb.append(arr.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<n; ++i){
+            if(!visited[i]){
+                visited[i] = true;
+                arr.add(i+1);
+                int idx = arr.size()-1;
+                nNm(n, m-1, arr);
+                visited[i] = false;
+                arr.remove(idx);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n];
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        nNm(n, m, arr);
+        System.out.println(sb.toString());
+    }
+}
---
+++ boj15650.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+// BOJ 15650 N과 M(2) <백트래킹 단계>
+public class boj15650 {
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+    public static int N, M;
+    public static int[] arr;
+
+    public static void nNm(int a, int depth){
+        if(M==depth){
+            for(int var : arr){
+                sb.append(var).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=a; i<=N; ++i){
+            arr[depth] = i;
+            nNm(i+1, depth+1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        nNm(1, 0);
+        System.out.println(sb.toString());
+    }
+}
