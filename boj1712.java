698bb29d5acb2d85ca7c0a3847492e6c2ac75251
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-01-29 13:44:15 +0900

    2021.01.29 - Basic Mathmatics 1

---
+++ boj1712.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+// BOJ 1712 손인북기점 <기본 수학 1단계> Basic Mathematics 1
+public class boj1712 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        int cnt = 1;
+        if(B >= C){
+            System.out.println(-1);
+            return;
+        }
+
+        if(A + B * cnt < C * cnt){
+            System.out.println(1);
+            return;
+        }
+
+        while(A + B * cnt > C * cnt){
+            cnt++;
+        }
+
+        if(A + B * cnt == C * cnt)
+            System.out.println(++cnt);
+        else
+            System.out.println(cnt);
+    }
+}
