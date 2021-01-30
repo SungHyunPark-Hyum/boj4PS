e8b6fb6eb84c0e67cb61a982a334e7e8bcba060f
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-01-30 15:43:45 +0900

    2021.01.30 - Basic Mathmatics 1

--- boj2839.java
+++ boj2839.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class boj2839 {
+import java.io.*;
+
+// BOJ 2839 설탕 배달 <기본 수학 1단계>
+public class boj2839 {
+    public static void func(int n){
+        int five_num = n/5;
+        int ans = -1;
+
+        while(ans == -1 && five_num >= 0){
+            int tmp = n - 5 * five_num;
+            if(tmp % 3 == 0){
+                ans = (tmp/3) + five_num;
+            }
+            five_num--;
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        func(N);
+    }
 }
