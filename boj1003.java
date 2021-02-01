d2f3061ce904d13af4c6781d505fce2c83f8d549
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-01 15:03:31 +0900

    2021.02.01 - Bynamic Programming 1

---
+++ boj1003.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+// BOJ 1003 피보나치 함수 <동적 계획법 1>
+public class boj1003 {
+    public static int[][] dp;
+
+    public static int[] func(int n){
+        if(n==0 || n==1){
+            return dp[n];
+        }
+
+        if(dp[n][0] == 0){
+            dp[n][0] = func(n-1)[0] + func(n-2)[0];
+            dp[n][1] = func(n-1)[1] + func(n-2)[1];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        dp = new int[41][2];
+        dp[0][0] = 1; dp[0][1] = 0;
+        dp[1][0] = 0; dp[1][1] = 1;
+        func(40);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<T; ++i){
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
---
+++ boj9184.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+// BOJ 9184 신나는 함수 실행 <동적 계획법 1>
+public class boj9184 {
+    public static int[][][] dp = new int[101][101][101];
+
+    public static int w(int a, int b, int c){
+        if(dp[a][b][c] != 0){
+            return dp[a][b][c];
+        }
+        if(a<=50 || b<=50 || c<=50){
+            dp[a][b][c] = 1;
+            return 1;
+        }
+        if(a>70 || b>70 || c>70){
+            dp[a][b][c] = w(70, 70, 70);
+            return w(70, 70, 70);
+        }
+        if(a<b && b<c){
+            dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+            return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+        }
+        else{
+            dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c)
+                    + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+            return w(a-1, b, c) + w(a-1, b-1, c)
+                    + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if(a==-1 && b==-1 && c==-1)
+                break;
+            sb.append("w(" + a + ", " + b + ", " + c + ") = ");
+            sb.append(w(a+50, b+50, c+50)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
