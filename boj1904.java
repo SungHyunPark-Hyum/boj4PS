18a60d74aa5692ff2988800b3d14d274b6974e35
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-03 16:13:05 +0900

    2021.02.03 - Dynamic Programming 2

---
+++ boj1904.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+// BOJ 1904 01타일 <동적 계획법 1>
+public class boj1904 {
+    public static int[] dp;
+    public static void func(int n){
+        dp[1] = 1;
+        if(n >= 2) dp[2] = 2;
+        if(n == 1 || n == 2){
+            System.out.println(dp[n]);
+            return;
+        }
+        for(int i=3; i<=n; ++i){
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+        }
+        System.out.println(dp[n]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N+1];
+        func(N);
+    }
+}
