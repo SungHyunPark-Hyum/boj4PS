1964143ee5e387422fd8cac08c30d89c39ac76f0
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-08 17:13:10 +0900

    not yet

--- boj10844.java
+++ boj10844.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class boj10844 {
+import java.io.*;
+
+// BOJ 10844 쉬운 계단 수 <동적 계획법>
+// not yet, try later
+public class boj10844 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        // 1 : 9
+        // 2 : 17 = dp[1] * 2 - 1;
+        // 3 : 32 = dp[2] * 2 - 2;
+        // 4 : 61 = dp[3] * 2 - 3;
+        int[] dp = new int[N+1];
+        dp[1] = 9;
+        for(int i=2; i<=N; ++i){
+            dp[i] = (dp[i-1] * 2 - (i-1)) % 1000000000;
+        }
+        System.out.println(dp[N]);
+    }
 }
