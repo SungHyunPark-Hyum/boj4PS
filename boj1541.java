33d013c21832b679100855d02bb9b74a2c949c50
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-08 15:54:34 +0900

    2021.02.08 - Greedy Algorithm

---
+++ boj1541.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+// BOJ 1541 잃어버린 괄호 <그리디 알고리즘>
+public class boj1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer nums = new StringTokenizer(str, "+|-");
+        String oprs = str.replaceAll("[0-9]", "");
+        int sum = Integer.parseInt(nums.nextToken());
+        boolean flag = false;
+        for(int i=0; i<oprs.length(); ++i){
+            if(oprs.charAt(i) == '+'){
+                if(!flag){
+                    sum += Integer.parseInt(nums.nextToken());
+                }
+                else{
+                    sum -= Integer.parseInt(nums.nextToken());
+                }
+            }
+            else{
+                flag = true;
+                sum -= Integer.parseInt(nums.nextToken());
+            }
+        }
+        System.out.println(sum);
+    }
+}
