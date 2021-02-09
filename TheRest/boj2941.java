3d3912642634f9f18915eb8fa2407863e7e3d8af
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-09 19:59:47 +0900

    2021.02.09 - String Method (replaceAll() & split)

---
+++ TheRest/boj2941.java
@@ -0,0 +1,12 @@
+package TheRest;
+
+import java.io.*;
+
+// BOJ 2941 크로아티아 알파벳 <문자열>
+public class boj2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", "!");
+        System.out.println(str.length());
+    }
+}
