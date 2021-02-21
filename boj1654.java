b79c7354fae483b2297fd0c381385c832d3b2a79
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-21 17:00:16 +0900

    nothing

--- boj1654.java
+++ boj1654.java
@@ -5,6 +5,6 @@ import java.util.*;
 public class boj1654 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        // today, I'll finish this question.
     }
 }
