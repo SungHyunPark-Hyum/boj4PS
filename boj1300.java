b2ff9eec5fd375c9f7d697fc194928336f38c8cb
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-02-25 15:05:53 +0900

    too hard for me, i'll do it later

--- boj1300.java
+++ boj1300.java
@@ -3,44 +3,15 @@ import java.util.*;
 
 // BOJ 1300 K번째 수 <이분 탐색>
 public class boj1300 {
-    public void binarySearch(int a, int[] arr){
-        int l = 1;
-        int r = arr.length-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(mid == a){
-                arr[a]++;
-            }
-            else if(mid < a){
-                l = mid + 1;
-            }
-            else{
-                r = mid - 1;
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[k+1];
-        int count = 0;
-
         for(long i=1; i<=N; ++i){
             for(long j=1; j<=N; ++j){
-                if(i*j > k) continue;
-                arr[(int)i*(int)j]++;
-                count++;
-            }
-        }
-        count = 0;
-        for(int i=1; i<=k; ++i){
-            count += arr[i];
-            if(count >= k){
-                System.out.println(i);
-                break;
+
             }
         }
         // 1  2  3  4  5  6
@@ -49,5 +20,11 @@ public class boj1300 {
         // 4  8 12 16 20 24
         // 5 10 15 20 25 30
         // 6 12 18 24 30 36
+
+        // k = 13 -> 9
+        // 1  2  3  4
+        // 2  4  6  8
+        // 3  6  9 12
+        // 4  8 12 16
     }
 }
