6e43281a3c4d3131d6a074f8bf9c4afbc3c74911
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-03-21 16:33:25 +0900

    2021.03.21 - MST with Prim

--- Tree/boj1197_Prim.java
+++ Tree/boj1197_Prim.java
@@ -1,3 +1,5 @@
+package Tree;
+
 import java.io.*;
 import java.util.*;
 
@@ -5,19 +7,40 @@ import java.util.*;
 public class boj1197_Prim {
     public static int V, E;
     public static int[] parent;
+    public static ArrayList<Edge>[] arr;
+    public static boolean[][] visit;
 
-    public static void kruskal(PriorityQueue<Edge> pq){
-        Edge start = pq.poll();
-        int ans = start.weight;
-        int count = 1;
-        union(start.a, start.b);
+    public static void prim(int a, int b, int w){
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>(new Comparator<Edge>(){
+            @Override
+            public int compare(Edge e1, Edge e2){
+                return e1.weight - e2.weight;
+            }
+        });
+        pq.add(new Edge(a, b ,w));
+        int ans = 0;
+        int count = 0;
 
         while(count != V-1){
             Edge e = pq.poll();
 
             if(union(e.a, e.b)){
+                visit[e.a][e.b] = true;
+                visit[e.b][e.a] = true;
                 count++;
                 ans += e.weight;
+                for(Edge ne : arr[e.a]){
+                    if(!visit[ne.a][ne.b]){
+                        visit[ne.a][ne.b] = true;
+                        visit[ne.b][ne.a] = true;
+                        pq.add(new Edge(ne.a, ne.b, ne.weight));
+                    }
+                }
+                for(Edge ne : arr[e.b]){
+                    visit[ne.a][ne.b] = true;
+                    visit[ne.b][ne.a] = true;
+                    pq.add(new Edge(ne.a, ne.b, ne.weight));
+                }
             }
         }
 
@@ -48,22 +71,26 @@ public class boj1197_Prim {
         StringTokenizer st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
+        visit = new boolean[V+1][V+1];
+        arr = new ArrayList[V+1];
+        for(int i=1; i<=V; ++i)
+            arr[i] = new ArrayList<Edge>();
         parent = new int[V+1];
         for(int i=1; i<=V; ++i)
             parent[i] = i;
 
-        PriorityQueue<Edge> pq = new PriorityQueue<Edge>(new Comparator<Edge>(){
-            @Override
-            public int compare(Edge e1, Edge e2){
-                return e1.weight - e2.weight;
-            }
-        });
+
+        int aa = 0, bb = 0, ww = Integer.MAX_VALUE;
         while(E-- > 0){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            pq.add(new Edge(a, b, w));
+            arr[a].add(new Edge(a, b, w));
+            arr[b].add(new Edge(b, a, w));
+            if(w < ww){
+                aa = a; bb = b; ww = w;
+            }
         }
         if(V == 1){
             System.out.println(0);
@@ -71,6 +98,7 @@ public class boj1197_Prim {
             return;
         }
 
+        prim(aa, bb, ww);
         br.close();
         return;
     }
