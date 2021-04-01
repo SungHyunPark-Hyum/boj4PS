e3e156e7d5ebfd04fb434ac3ffef15337e7fbab0
SungHyunPark-Hyum <fpekrkfk@gmail.com>
2021-04-01 16:24:32 +0900

    2021.04.01 - [1차] 길찾기 게임 - organize tree

---
+++ a_Programmers/KAKAO_BLIND_RECRUITMENT_2018/Level3_ShuttleBus.java
@@ -0,0 +1,18 @@
+package a_Programmers.KAKAO_BLIND_RECRUITMENT;
+
+import java.io.*;
+import java.util.*;
+
+// Programmers [1차] 셔틀버스
+public class Level3_ShuttleBus {
+
+    public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
